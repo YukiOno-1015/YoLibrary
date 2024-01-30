efa195d41d5db2a21cf9688d4067f2facd580ffb
yuki-ono-vlb <yuki.ono@lberc-g.jp>
2024-01-31 03:23:06 +0900

    Utils修正

--- Sources/YoLibrary/Utils.swift
+++ Sources/YoLibrary/Utils.swift
@@ -1,17 +1,17 @@
 //
 //  Utils.swift
-//  
+//
 //
 //  Created by yukiono on 2024/01/27.
 //
 
 import Foundation
 public class Utils {
-    public func postApnsPayload(userInfo:[AnyHashable: Any]) {
+    public static func postApnsPayload(_ userInfo: Dictionary<String, Any?>) {
         dump(userInfo)
     }
     
-    public func lStr(keyCode: String) -> String {
+    public static func lStr(keyCode: String) -> String {
         return NSLocalizedString(keyCode, comment: "")
     }
     
