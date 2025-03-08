b8ec01a454dd54dd922f7d666403b93a1a42f5a3
yuki-ono-vlb <yuki.ono@lberc-g.jp>
2025-03-09 06:42:37 +0900

    多言語対応

--- Sources/YoLibrary/Bundle+Extension.swift
+++ Sources/YoLibrary/Bundle+Extension.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 public extension Bundle {
+    /// **`Locale` の言語コードを `ja` などの一般的な形式に変換**
+    ///
+    /// - Returns: `"ja"`, `"en"` などの基本的な言語コード
+    static var preferredLanguageCode: String {
+        let locale = Locale.preferredLanguages.first ?? "en"
+        return Locale(identifier: locale).languageCode ?? "en" // `ja-JP` → `ja`
+    }
+
     static var yoLibrary: Bundle {
         let bundle = Bundle.module
-        let preferredLanguage = Locale.preferredLanguages.first ?? "en"
+        let preferredLanguage = preferredLanguageCode ?? "en"
 
         Logger.debug(message: "🌏 現在の言語: \(preferredLanguage)")
         Logger.debug(message: bundle.path(forResource: preferredLanguage, ofType: "lproj") ?? "なし")
